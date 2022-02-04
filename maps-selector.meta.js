144f5020daf5bb5d78f11d03ff8d091a9cbe93be
Nux <eccenux+git@gmail.com>
2022-02-04 23:19:44 +0100

    0.0.2: intel url update

--- maps-selector.meta.js
+++ maps-selector.meta.js
@@ -2,9 +2,9 @@
 // @id             iitc-plugin-maps-selector@eccenux
 // @name           IITC plugin: Quick selector of maps
 // @category       Misc
-// @version        0.0.1
+// @version        0.0.2
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    [0.0.1] This plugin provides extra toolbar allowing a quick change of map layers (Ingress, Roads, Hybrid, Terrain).
+// @description    [0.0.2] This plugin provides extra toolbar allowing a quick change of map layers (Ingress, Roads, Hybrid, Terrain).
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
@@ -13,5 +13,6 @@
 // @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
--- maps-selector.user.js
+++ maps-selector.user.js
@@ -2,9 +2,9 @@
 // @id             iitc-plugin-maps-selector@eccenux
 // @name           IITC plugin: Quick selector of maps
 // @category       Misc
-// @version        0.0.1
+// @version        0.0.2
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    [0.0.1] This plugin provides extra toolbar allowing a quick change of map layers (Ingress, Roads, Hybrid, Terrain).
+// @description    [0.0.2] This plugin provides extra toolbar allowing a quick change of map layers (Ingress, Roads, Hybrid, Terrain).
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
@@ -13,6 +13,7 @@
 // @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // @updateURL      https://github.com/Eccenux/iitc-plugin-maps-selector/raw/master/maps-selector.meta.js
 // @downloadURL    https://github.com/Eccenux/iitc-plugin-maps-selector/raw/master/maps-selector.user.js
